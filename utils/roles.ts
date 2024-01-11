ce3739a0f94bab51671a12e703f9f43bbc060bbf
Daniel Lima <daniel.lima@orium.network>
2024-01-11 15:22:27 -0300

    ON-549: Accept Rental Offer

--- utils/roles.ts
+++ utils/roles.ts
@@ -1,3 +1,4 @@
 import { ethers } from 'ethers'
 
 export const USER_ROLE = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('USER_ROLE'))
+export const UNIQUE_ROLE = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('UNIQUE_ROLE'))
