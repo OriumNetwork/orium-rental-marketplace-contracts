b86b5ea97b87caaa4d999ece29073c0824f645f8
Daniel Lima <daniel.lima@orium.network>
2024-03-01 14:25:35 -0300

    ON-688: add more tests

--- test/foundry/OriumSftMarketplaceTest.sol
+++ test/foundry/OriumSftMarketplaceTest.sol
@@ -4,7 +4,6 @@ pragma solidity 0.8.9;
 
 import { SetupTest } from "./SetupTest.sol";
 import { RentalOffer } from "../../contracts/libraries/LibOriumSftMarketplace.sol";
-import "hardhat/console.sol";
 
 contract OriumSftMarketplaceTest is SetupTest {
     uint256 nonceCounter;
@@ -15,8 +14,8 @@ contract OriumSftMarketplaceTest is SetupTest {
     }
 
     function test_fuzz_createRentalOffer(address lenderFuzz, address borrowerFuzz) public {
-        vm.assume(lenderFuzz != address(0));
-        vm.assume(borrowerFuzz != address(0));
+        vm.assume(lenderFuzz != address(0) && lenderFuzz.code.length == 0);
+        vm.assume(borrowerFuzz != address(0) && borrowerFuzz.code.length == 0);
 
         bytes32[] memory roles = new bytes32[](1);
         roles[0] = UNIQUE_ROLE;
@@ -48,8 +47,8 @@ contract OriumSftMarketplaceTest is SetupTest {
     }
 
    function test_fuzz_acceptRentalOffer(address lenderFuzz, address borrowerFuzz, uint64 duration) public {
-        vm.assume(lenderFuzz != address(0) && lenderFuzz != address(this));
-        vm.assume(borrowerFuzz != address(0) && borrowerFuzz != address(this));
+        vm.assume(lenderFuzz != address(0) && lenderFuzz.code.length == 0);
+        vm.assume(borrowerFuzz != address(0) && borrowerFuzz.code.length == 0);
         uint64 deadline = uint64(block.timestamp + 30 days);
         vm.assume(duration > block.timestamp && duration < deadline);
 
@@ -74,20 +73,6 @@ contract OriumSftMarketplaceTest is SetupTest {
             rolesData: rolesData
         });
 
-        console.log("Offer: ");
-        console.log("lender: ", _offer.lender);
-        console.log("borrower: ", _offer.borrower);
-        console.log("tokenAddress: ", _offer.tokenAddress);
-        console.log("tokenId: ", _offer.tokenId);
-        console.log("tokenAmount: ", _offer.tokenAmount);
-        console.log("feeTokenAddress: ", _offer.feeTokenAddress);
-        console.log("feeAmountPerSecond: ", _offer.feeAmountPerSecond);
-        console.log("nonce: ", _offer.nonce);
-        console.log("commitmentId: ", _offer.commitmentId);
-        console.log("deadline: ", _offer.deadline);
-        console.logBytes32(_offer.roles[0]);
-        console.logBytes(_offer.rolesData[0]);
-
         vm.startPrank(lenderFuzz);
         sft.mint(lenderFuzz, tokenId, tokenAmount, "");
         sft.setApprovalForAll(address(rolesRegistry), true);
@@ -102,7 +87,44 @@ contract OriumSftMarketplaceTest is SetupTest {
         vm.stopPrank();
     }
 
-     function invariant_offerHash() public {
+    function test_fuzz_cancelRentalOffer(address lenderFuzz) public {
+        vm.assume(lenderFuzz != address(0) && lenderFuzz.code.length == 0);
+
+        bytes32[] memory roles = new bytes32[](1);
+        roles[0] = UNIQUE_ROLE;
+
+        bytes[] memory rolesData = new bytes[](1);
+        rolesData[0] = "0x";
+
+        RentalOffer memory _offer = RentalOffer({
+            lender: lenderFuzz,
+            borrower: address(0),
+            tokenAddress: address(sft),
+            tokenId: tokenId,
+            tokenAmount: tokenAmount,
+            feeTokenAddress: address(feeToken),
+            feeAmountPerSecond: 1,
+            nonce: ++nonceCounter,
+            commitmentId: 0,
+            deadline: uint64(block.timestamp + 1 days),
+            roles: roles,
+            rolesData: rolesData
+        });
+
+        vm.startPrank(lenderFuzz);
+        sft.mint(lenderFuzz, tokenId, tokenAmount, "");
+        sft.setApprovalForAll(address(rolesRegistry), true);
+        rolesRegistry.setRoleApprovalForAll(address(sft), address(marketplace), true);
+        marketplace.createRentalOffer(_offer);
+        vm.stopPrank();
+
+        vm.startPrank(lenderFuzz);
+        _offer.commitmentId = ++commitmentIdCounter;
+        marketplace.cancelRentalOffer(_offer);
+        vm.stopPrank();
+    }
+
+    function invariant_offerHash() public {
         bytes32[] memory roles = new bytes32[](1);
         roles[0] = UNIQUE_ROLE;
 
